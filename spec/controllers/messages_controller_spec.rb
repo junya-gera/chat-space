1be6f3f4255360ac0d3d21a7059465935e226f51
junya-gera <aa106921@gmail.com>
2020-05-11 19:00:37 +0900

    fix message_controller_spec.rb

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,38 +1,43 @@
 require 'rails_helper'
 
 describe MessagesController do
-  let(:group) { create(:group) }
-  let(:user) { create(:user) }
 
   describe '#index' do
+    # メッセージ一覧でテストすべき点
+      # ログインしている場合
+        # アクション内で定義しているインスタンス変数があるか
+        # 該当するビューが描画されているか
+      # ログインしていない場合
+        # 意図したビューにリダイレクトしているか
 
-    context 'log in' do
+    # 複数のexample内で同一のインスタンスを使用したい場合、letメソッドを使う
+    # letメソッドは初回の呼び出し時のみ実行される（遅延評価）。繰り返しがないので高速
+    # 
+    let(:group) { create(:group) }
+    let(:user) { create(:user) }
+
+    context 'ログインしている場合' do
+      # beforeメソッドは、各exampleが実行される直前に、毎回実行される。ここに共通の処理をまとめておく
       before do
-        login user
+        # ここのloginメソッドはmacrosで定義したもの
+        login user  
+        # 擬似的にindexアクションを動かすリクエストを行うためにgetメソッドを使用
+        # messagesはgroupsにネストしているので、group_idが必要。getメソッドの引数にparams以下を渡している
         get :index, params: { group_id: group.id }
       end
-
-      it 'assigns @message' do
+      it '@messageに期待した値が入っていること' do
         expect(assigns(:message)).to be_a_new(Message)
       end
 
-      it 'assigns @group' do
+      it '@groupに期待した値が入っていること' do
         expect(assigns(:group)).to eq group
-      end
-
-      it 'renders index' do
-        expect(response).to render_template :index
       end
     end
 
-    context 'not log in' do
+    context 'ログインしていない場合' do
       before do
         get :index, params: { group_id: group.id }
       end
-
-      it 'redirects to new_user_session_path' do
-        expect(response).to redirect_to(new_user_session_path)
-      end
     end
-  end
+
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -39,37 +39,4 @@ RSpec.describe Message, type: :model do
       end
     end
   end
-
-  describe '#index' do
-    # メッセージ一覧でテストすべき点
-      # ログインしている場合
-        # アクション内で定義しているインスタンス変数があるか
-        # 該当するビューが描画されているか
-      # ログインしていない場合
-        # 意図したビューにリダイレクトしているか
-
-    # 複数のexample内で同一のインスタンスを使用したい場合、letメソッドを使う
-    # letメソッドは初回の呼び出し時のみ実行される（遅延評価）。繰り返しがないので高速
-    # 
-    let(:group) { create(:group) }
-    let(:user) { create(:user) }
-
-    context 'ログインしている場合' do
-      # beforeメソッドは、各exampleが実行される直前に、毎回実行される。ここに共通の処理をまとめておく
-      before do
-        # ここのloginメソッドはmacrosで定義したもの
-        login user  
-        # 擬似的にindexアクションを動かすリクエストを行うためにgetメソッドを使用
-        # messagesはgroupsにネストしているので、group_idが必要。getメソッドの引数にparams以下を渡している
-        get :index, params: { group_id: group.id }
-      end
-    end
-
-    context 'ログインしていない場合' do
-      before do
-        get :index, params: { group_id: group.id }
-      end
-    end
-
-  end
 end
