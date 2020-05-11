bab0898eadd7dfea7a938845d1fec81b70d1afc8
junya-gera <aa106921@gmail.com>
2020-05-11 19:11:42 +0900

    completed message-index-test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -25,19 +25,36 @@ describe MessagesController do
         # messagesはgroupsにネストしているので、group_idが必要。getメソッドの引数にparams以下を渡している
         get :index, params: { group_id: group.id }
       end
+
+
       it '@messageに期待した値が入っていること' do
+        # assigns(:message)とする事で@messageの中身を確認できる
+        # @messageはMessage.newで定義された新しいMessageクラスのインスタンス
+        # be_a_newマッチャを使う事で()で指定したクラスのインスタンスかつ未保存のレコードであるかを確かめる
         expect(assigns(:message)).to be_a_new(Message)
       end
 
       it '@groupに期待した値が入っていること' do
         expect(assigns(:group)).to eq group
       end
+
+      it 'index.html.erb に遷移すること' do
+        # responseは、example内でリクエストが行われた後の遷移先のビューの情報を持つインスタンス
+        # render_templateマッチャは引数にアクション名をとり、引数で指定されたアクションがリクエストされた時に自動的に遷移するビューを返す
+        expect(response).to render_template :index
+      end
+
     end
 
     context 'ログインしていない場合' do
       before do
         get :index, params: { group_id: group.id }
       end
-    end
 
+      it 'ログイン画面にリダイレクトすること' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+      
+    end
+  end
 end
